1f6cb80b4e4433cc44905349fc0cef372f613144
Giblin <p.giblin97@gmail.com>
2025-02-16 13:24:15 -0600

    Starting files

--- Scripts/Character.cs
+++ Scripts/Character.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public partial class Character : R
+public partial class Character : PhysicsBody2D
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
