7f79113381f13ada48d8c9c029deb9889bed5330
Giblin <p.giblin97@gmail.com>
2025-02-16 18:49:27 -0600

    AHHHHHHH

--- Scripts/Weapon.cs
+++ Scripts/Weapon.cs
@@ -1,15 +1,45 @@
-using Godot;
-using System;
-
-public partial class Weapon : CollisionShape2D
+public partial class Weapon : Node2D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+    [Export]
+    public int Damage { get; private set; } = 1;
+    [Export]
+    public int Speed { get; private set; } = 1;
+    [Export]
+    public int Cooldown { get; private set; } = 0;
+
+    public WeaponState State { get; private set; } = WeaponState.Ready;
+
+    private double cooldown;
+
+    public override void _Process(double delta)
 	{
+        switch (State)
+        {
+            case WeaponState.Cooldown:
+                cooldown -= delta;
+                if (cooldown <= 0)
+                {
+                    State = WeaponState.Ready;
+                }
+                return;
+            default:
+                return;
+        }
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    public void Attack()
+    {
+        if (State == WeaponState.Ready)
+        {
+            State = WeaponState.Attacking;
+            cooldown = Cooldown;
+        }
+    }
+
+    public enum WeaponState
+    {
+        Ready,
+        Attacking,
+        Cooldown
+    }
 }
