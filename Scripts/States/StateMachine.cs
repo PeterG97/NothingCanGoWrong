c8227fc8596c98a15c500bbc9a85661a995e1ba7
Giblin <p.giblin97@gmail.com>
2025-02-17 10:08:57 -0600

    a

--- Scripts/States/StateMachine.cs
+++ Scripts/States/StateMachine.cs
@@ -4,71 +4,71 @@ using System;
 
 public partial class StateMachine : Node
 {
-	[Export]
-	public State? IntialState {get; set;}
+    //[Export]
+    //public State? IntialState {get; set;}
 
-	public Dictionary<string,State> states { get; set;} = new ();
-	private State? currentState;
-	
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		foreach (var child in GetChildren())
-		{
-			if (child is State state)
-			{
-				states[child.Name.ToString().ToLower()] = state;
+    //public Dictionary<string,State> states { get; set;} = new ();
+    //private State? currentState;
 
-				//potential mememory leak but fuck it we ball
-				state.Transistioned += () => OnChildTransition(state, state.Name);
-				
-			}
-		}
-		if (IntialState is not null)
-		{
-			IntialState.Enter();
-			currentState = IntialState;
-		}
-	}
+    //// Called when the node enters the scene tree for the first time.
+    //public override void _Ready()
+    //{
+    //	foreach (var child in GetChildren())
+    //	{
+    //		if (child is State state)
+    //		{
+    //			states[child.Name.ToString().ToLower()] = state;
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-		if (currentState is not null)
-		{
-			currentState.Update();
-		}
-	}
+    //			//potential mememory leak but fuck it we ball
+    //			state.Transistioned += () => OnChildTransition(state, state.Name);
 
-	public override void _PhysicsProcess(double delta)
-	{
-		if (currentState is not null)
-		{
-			currentState.PhysicsUpdate(delta);
-		}
-	}
+    //		}
+    //	}
+    //	if (IntialState is not null)
+    //	{
+    //		IntialState.Enter();
+    //		currentState = IntialState;
+    //	}
+    //}
 
-	public void OnChildTransition(State state, string newStateName)
-	{
-		if (state != currentState)
-		{
-			return;
-		}
-		
-		var newState = states[newStateName.ToLower()];
-		if (newState is null)
-		{
-			return;
-		}
+    //// Called every frame. 'delta' is the elapsed time since the previous frame.
+    //public override void _Process(double delta)
+    //{
+    //	if (currentState is not null)
+    //	{
+    //		currentState.Update();
+    //	}
+    //}
 
-		if (currentState is not null)
-		{
-			currentState.Exit();
-		}
+    //public override void _PhysicsProcess(double delta)
+    //{
+    //	if (currentState is not null)
+    //	{
+    //		currentState.PhysicsUpdate(delta);
+    //	}
+    //}
 
-		newState.Enter();
-		currentState = newState;
-	}
+    //public void OnChildTransition(State state, string newStateName)
+    //{
+    //	if (state != currentState)
+    //	{
+    //		return;
+    //	}
+
+    //	var newState = states[newStateName.ToLower()];
+    //	if (newState is null)
+    //	{
+    //		return;
+    //	}
+
+    //	if (currentState is not null)
+    //	{
+    //		currentState.Exit();
+    //	}
+
+    //	newState.Enter();
+    //	currentState = newState;
+    //}
 
 
 
